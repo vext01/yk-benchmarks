eb77aed51790dc7772882339c457e13a0dbef2e1
Edd Barrett <vext01@gmail.com>
2024-07-31 13:54:42 +0100

    Simulate no git installed.

--- .buildbot.sh
+++ .buildbot.sh
@@ -2,7 +2,14 @@
 
 set -eu
 
-pipx install rebench
+wget https://github.com/smarr/ReBench/archive/refs/heads/dont-require-git.zip
+unzip dont-require-git.zip
+mv ReBench-dont-require-git rebench
+cd rebench
+pipx install .
+cd ..
+
+export PATH=~/.local/pipx/venvs/rebench/bin:$PATH
 
 # Do a "quick" run as a smoke-test.
-~/.local/bin/rebench --quick --no-denoise -c rebench.conf
+rebench --quick --no-denoise -c rebench.conf
